670cbf1966a68f20b2a90d8c68cffb2a582bd9ad
Untitled <196356904+untitled-guy@users.noreply.github.com>
2025-02-19 18:36:49 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,4 @@
+import random
+quotes = ("Be yourself; everyone else is already taken","Two things are infinite: the universe and human stupidity, and I'm not sure about the universe.","A room without books is like a body without a soul.","You know you're in love when you can't fall asleep because reality is finally better than your dreams.","You only live once, but if you do it right, once is enough.","In three words I can sum up everything I've learned about life: it goes on.","If you tell the truth, you don't have to remember anything.")
+quote = random.choice(quotes)
+print(quote)
